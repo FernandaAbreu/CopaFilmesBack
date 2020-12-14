053be3aa48f75fd22f857098e0afacec44195b2f
Fernanda Abreu <fefetwsp@hotmail.com>
2020-12-14 10:34:22 -0300

    Add feature to generate top movies by user selection

---
+++ CopaFilmesBackEnd/Controllers/MovieController.cs
@@ -0,0 +1,68 @@
+﻿
+using System;
+using System.Collections.Generic;
+using CopaFilmesBackEnd.Extensions;
+using CopaFilmesBackEnd.Helpers;
+using CopaFilmesBackEnd.Services.Interfaces;
+using CopaFilmesBackEnd.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+namespace CopaFilmesBackEnd.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+        IMovieService mMovieService;
+        private readonly ILogger logger;
+
+        public MovieController(IMovieService movieService, ILogger<MovieController> logger)
+        {
+            mMovieService = movieService;
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<VMMovie>> GetAll()
+        {
+            try
+            {
+                var movies = mMovieService.GetAll();
+                return Ok(movies);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("Internal Error" + ex.Message);
+                return StatusCode(500, new { error = "Internal server error" });
+            }
+        }
+
+
+
+        [HttpPost]
+        public ActionResult<List<VMMovie>> Post([FromBody] List<VMMovie> listMovies)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState.GetErrorMessages());
+                }
+
+                return mMovieService.generatePositionsBySomeMovies(listMovies);
+            }
+            catch (CustomHttpException ex)
+            {
+                logger.LogError("Exception expected" + ex.Message);
+                return StatusCode(ex.StatusCode, ex.ErrorMessage);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("Internal Error" + ex.Message);
+                return StatusCode(500, new { error = "Internal server error" });
+            }
+
+        }
+    }
+}
--- CopaFilmesBackEnd/Startup.cs
+++ CopaFilmesBackEnd/Startup.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using CopaFilmesBackEnd.CrossCutting.DependencyInjection;
+using CopaFilmesBackEnd.Helpers;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +14,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 namespace CopaFilmesBackEnd
 {
@@ -25,8 +31,28 @@ namespace CopaFilmesBackEnd
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddOptions();
+            var appSettings = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettings);
+            ConfigureService.ConfigureDependenciesServices(services);
+            services.AddSingleton<IConfiguration>(Configuration);
+            services.AddCors();
             services.AddControllers();
-        }
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "Copa Filmes",
+                    Version = "v1",
+                    Description = "Copa Filmes WebAPI"
+
+                });
+
+                c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+            }
+            );
+        }   
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -39,9 +65,17 @@ namespace CopaFilmesBackEnd
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors(x => x
+              .AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader());
             app.UseAuthorization();
+            app.UseSwagger();
 
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Copa Filmes WebAPI");
+
+            });
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
